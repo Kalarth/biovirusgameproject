cdb3b5f7aa4c99d5ccc9f5cd73e30892226e5ce5
Pierre Jacquet <peepanjacquet@gmail.com>
2017-11-11 12:22:36 +0100

    update

--- viruskiller.py
+++ viruskiller.py
@@ -1,31 +1,118 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import random
+from tkinter import *
+
+
 
 os.system('clear')
-print("Debut du programme !",'\n')
-x=[1,2,3,4,5,6,7,8,9,10]
-y=[0,10,20,30,40,50,60,70,80,90]
 
-grille=[]
-grillex=[]
-separator="---------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
-#Creation d'une liste sous la forme [[1,2,3,4,5,6,7,8,9,10],[11,12,13,14...]]
-for i in range(len(y)):
-    grillex=[]
-    for j in range(len(x)):
-        value=x[j]+y[i]
-        grillex.append(value)
-    grille.append(grillex)
+#COLORS
+VIOLET = '\033[95m'
+BLEU = '\033[94m'
+VERT = '\033[92m'
+ORANGE = '\033[93m'
+ROUGE = '\033[91m'
+BLANC = '\033[0m'
+GRAS = '\033[1m'
+SOULIGNE = '\033[4m'
 
-#Print de la liste avec un retour a la ligne entre chaque sous-liste et tabulation pour chaque elements
-print ("\n",separator)
-for i in range(len(grille)):                                    #on boucle à travers les listes "dizaine" 0. 1. 2. 3. [...]
-    print("|\t\t"*(len(grille[j])+1))                           #print une ligne de "| |" vide avant et après les cases
-    print ('|',end="\t")
-    for j in range(len(grille[j])):                             #on boucle à travers les sous listes "unité" .0 .1 .2 .3 [...]
-        print(grille[i][j],"\t|",end='\t')
-    print("\n",end="")                                          #saute une ligne et laisse le texte du prochain print après
-    print("|\t\t"*(len(grille[j])+1))                           #print une ligne de "| |" vide avant et après les cases
-    print ("",separator)
+casevirus=BLEU+"\t(◣_◢)\t"+BLANC
+casevide=ROUGE+"\t   .   \t"+BLANC
+casejoueur=VERT+"\t(⌐■_■)\t"+BLANC
+
+grille=[ROUGE+"\t   .   \t"+BLANC]*100
+
+
+
+print (ORANGE + "DEBUT DU PROGRAMME (en couleur)","\n"
+      + BLANC)
+
+
+def showGameBoard(grille):
+    os.system('clear')
+    for i in range(0,100):
+        print (grille[i],end='')
+        if i!=0 and i%10==9:
+            print ("\n\n\n")
+
+
+
+def positionvirus():
+    virus=random.sample(range(0,99),4) #list of 4 different number
+    vir1=virus[0]
+    vir2=virus[1]
+    vir3=virus[2]
+    vir4=virus[3]
+    print ("Position des virus: ",ROUGE,vir1,vir2,vir3,vir4,BLANC+"\n")
+    grille[vir1]=casevirus
+    grille[vir2]=casevirus
+    grille[vir4]=casevirus
+    grille[vir3]=casevirus
+
+
+def initjoueur():
+    joueur=position[1]
+    grille[joueur]=casejoueur
+
+
+def keyinput(position):
+    oldpos=position[1]
+    inputkey=input("Saisissez votre direction:  ")
+    if inputkey=="z":
+        newpos=oldpos-10
+        position=[oldpos,newpos]
+        return position
+
+    if inputkey=="q":
+        newpos=oldpos-1
+        position=[oldpos,newpos]
+        return position
+
+
+    if inputkey=="s":
+        newpos=oldpos+10
+        position=[oldpos,newpos]
+        return position
+
+
+    if inputkey=="d":
+        newpos=oldpos+1
+        position=[oldpos,newpos]
+        return position
+
+
+def movejoueur(position,continuer):
+        testposition=keyinput(position)
+        oldpos=testposition[0]
+        newpos=testposition[1]
+
+        if grille[newpos] != casevide:
+            print ("Vous ne pouvez pas avancer plus loin")
+            print (position)
+            continuer=0
+            return position
+
+        if grille[newpos] == casevide:
+            grille[oldpos]=casevide
+            grille[newpos]=casejoueur
+            position=[oldpos,newpos]
+            showGameBoard(grille)
+            print (position)
+            return position
+
+
+#MAIN
+
+position=[0,random.randint(0,99)]
+print (position)
+continuer=1
+
+positionvirus()
+initjoueur()
+showGameBoard(grille)
+
+while continuer==1:
+    position=movejoueur(position,continuer)
